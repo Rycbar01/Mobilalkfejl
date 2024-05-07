8195b9069c311c721c8296497664f81998357730
Rycbar01 <zsuca980@gmail.com>
2024-05-07 15:19:32 +0200

    Kezdes

--- app/src/main/java/com/example/konyvwebshop/MainActivity.java
+++ app/src/main/java/com/example/konyvwebshop/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.konyvwebshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void bejelentkezes(View view) {
+        EditText userN = findViewById(R.id.edittextuserN);
+        EditText passW = findViewById(R.id.edittextpassword);
+
+
     }
 }
