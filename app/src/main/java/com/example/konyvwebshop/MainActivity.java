954d6cccd65d6827deb6294dc9d9f4373633fe4a
Rycbar01 <zsuca980@gmail.com>
2024-05-07 22:09:55 +0200

    1-3. vidi (3.v. 49.percig)

--- app/src/main/java/com/example/konyvwebshop/MainActivity.java
+++ app/src/main/java/com/example/konyvwebshop/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,11 +20,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userNET = findViewById(R.id.edittextuserN);
+        passWET = findViewById(R.id.edittextpassword);
+
+
+        Log.i(LOG_TAG, "onCreate");
     }
 
     public void bejelentkezes(View view) {
-        userNET = findViewById(R.id.edittextuserN);
-        passWET = findViewById(R.id.edittextpassword);
 
     String userN = userNET.getText().toString();
     String passW = passWET.getText().toString();
@@ -35,5 +40,35 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, RegisztActivity.class);
         intent.putExtra("SECRET_KEY", 99);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(LOG_TAG, "onResume");
     }
 }
--- app/src/main/java/com/example/konyvwebshop/RegisztActivity.java
+++ app/src/main/java/com/example/konyvwebshop/RegisztActivity.java
@@ -3,9 +3,18 @@ package com.example.konyvwebshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class RegisztActivity extends AppCompatActivity {
+    private static final String LOG_TAG = RegisztActivity.class.getName();
+
+    EditText usernamedittext;
+    EditText useremailedittext;
+    EditText passwedittext;
+    EditText passwagain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +26,74 @@ public class RegisztActivity extends AppCompatActivity {
         if(secret_key!=99){
             finish();
         }
+
+
+        usernamedittext = findViewById(R.id.usernameedittext);
+        useremailedittext = findViewById(R.id.useremailedittext);
+        passwedittext = findViewById(R.id.passwedittext);
+        passwagain = findViewById(R.id.passwagain);
+
+        Log.i(LOG_TAG, "onCreate");
+
     }
 
     public void regiszt(View view) {
+        String userN = usernamedittext.getText().toString();
+        String email = useremailedittext.getText().toString();
+        String passW = passwedittext.getText().toString();
+        String passWagain = passwagain.getText().toString();
+
+
+        if(!passW.equals(passWagain)){
+            Log.e(LOG_TAG, "A megadott jelszavak nem egyeznek.");
+            return;
+        }
+
+        Log.i(LOG_TAG, "Regisztrált: " + userN + ", e-mail: " + email);
     }
 
     public void megse(View view) {
         finish();
+    }
+
+    @Override
+    public void addContentView(View view, ViewGroup.LayoutParams params) {
+        super.addContentView(view, params);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(LOG_TAG, "onRestart");
     }
 }
